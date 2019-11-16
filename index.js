6e009946ca98fdf8fb253040cd8022698bb92e67
Rupinder Dhillon <rupinder.dhillon@flatironschool.com>
2019-11-16 03:27:23 +0000

    Done.

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ function getToken() {
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
   //use fetch to fork it!
+  const url = `https://api.github.com/repos/${repo}/forks`;
+
+  fetch(url, { method: 'POST', headers: { Authorization: 'djdkfjdkfjdkjf' } })
+  .then(res => res.json())
+  .then(json => showResults(json))
+  .catch(err => console.log(err));
 }
 
 function showResults(json) {
@@ -15,8 +21,20 @@ function showResults(json) {
 
 function createIssue() {
   //use this function to create an issue based on the values input in index.html
+  const url = 'https://api.github.com/repos/js-ajax-fetch-lab/issues';
+
+  fetch(url, { method: 'POST', headers: { Authorization: 'djdkfjdkfjdkjf' }, body: 'test body' })
+  .then(res => res.json())
+  .then(json => console.log(json))
+  .catch(err => console.log(err));
 }
 
 function getIssues() {
   //once an issue is submitted, fetch all open issues to see the issues you are creating
+  const url = 'https://api.github.com/repos/js-ajax-fetch-lab/issues';
+
+  fetch(url)
+  .then(res => res.json())
+  .then(json => console.log(json))
+  .catch(err => console.log(err));
 }
